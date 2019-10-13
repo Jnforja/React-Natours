a018690649cb62a4411b8f62a37f84f196734aa3
João Forja <joao.forja@challenge.pt>
2019-10-13 22:31:51 +0100

    Updates

---
+++ precache-manifest.25eaee38b6be4730f55ef42ad220b80c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5c70bdae6768675eba72bf6821fdae",
+    "url": "/React-Natours/index.html"
+  },
+  {
+    "revision": "35ea9c89a55b2d6e12b9",
+    "url": "/React-Natours/static/css/main.b1d5a71a.chunk.css"
+  },
+  {
+    "revision": "a7669861b1e97c7b129b",
+    "url": "/React-Natours/static/js/2.36d3cb18.chunk.js"
+  },
+  {
+    "revision": "35ea9c89a55b2d6e12b9",
+    "url": "/React-Natours/static/js/main.1ee0fcae.chunk.js"
+  },
+  {
+    "revision": "405758de07cfe67a2ec2",
+    "url": "/React-Natours/static/js/runtime-main.1d24b638.js"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/React-Natours/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/React-Natours/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/React-Natours/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/React-Natours/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/React-Natours/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "3365abba6477c48e6be892cf6faa8f25",
+    "url": "/React-Natours/static/media/logo-green-2x.3365abba.png"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/React-Natours/static/media/logo-white.40d4ef82.png"
+  },
+  {
+    "revision": "7fdf2a886516e020e300ae6bd544ab7d",
+    "url": "/React-Natours/static/media/nat-1-large.7fdf2a88.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/React-Natours/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/React-Natours/static/media/nat-2-large.7a25f199.jpg"
+  },
+  {
+    "revision": "417d3d741a7a47b3d6ef0307228033a0",
+    "url": "/React-Natours/static/media/nat-3-large.417d3d74.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/React-Natours/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/React-Natours/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/React-Natours/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/React-Natours/static/media/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/React-Natours/static/media/nat-8.14efd82b.jpg"
+  },
+  {
+    "revision": "b525a99bcf20d08b2aaf6dd83b7be623",
+    "url": "/React-Natours/static/media/nat-9.b525a99b.jpg"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/React-Natours/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/React-Natours/static/media/video.ffcd876c.mp4"
+  }
+]);
